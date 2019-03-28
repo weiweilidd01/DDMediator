a6a4397f69f13d22be83e2db2b3bad46982bd7c7
weiweili <weiweili@hk01.com>
2019-03-28 14:44:53 +0800

    update

--- Demo/Pods/Target Support Files/DDKit/DDKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DDKit : NSObject
-@end
-@implementation PodsDummy_DDKit
-@end
--- Demo/Pods/Target Support Files/Pods-DDMediatorDemo/Pods-DDMediatorDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DDMediatorDemo : NSObject
-@end
-@implementation PodsDummy_Pods_DDMediatorDemo
-@end
